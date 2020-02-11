32438fef051e8f86e6d18615cb408dfc8fe492de
Athelia Crosmun <athelia.hk@gmail.com>
2020-02-11 09:07:35 -0600

    Add past_pairs filtering and preliminary write-to-file logic (untested)

--- make_pairs.py
+++ make_pairs.py
@@ -35,15 +35,16 @@ Out: list of student pairs for today
         > Way Later: send students a slack message with their pair for today
 
 """
+FILE = 't1_pairs_dict.py'
 
 from t1_pairs_dict import STUDENT_PAIRS
 from pprint import pprint
 import random
 import itertools
 
-# Get random pairs, ignoring all factors
 def generate_pairs():
-    """Brute force, match students randomly"""
+    """Match students based on exclusion list and tech level"""
+
     students = set(STUDENT_PAIRS.keys())
     pairs = {}
 
@@ -55,29 +56,57 @@ def generate_pairs():
         try:
             match = (students - STUDENT_PAIRS[student]['excluded']).pop()
 
+            while match in STUDENT_PAIRS[student]['past_pairs']:
+            # try:
+                allowed_students = (students - STUDENT_PAIRS[student]['excluded']).pop()
+                match = allowed_students.pop()
+                print(f'Trying student={student} with match={match}')
+                # except:
+                #     print(f'Failed: students={students} student={student} match={match}')
+                #     return
+
             # Is the difference in levels within 3?
             if not abs(STUDENT_PAIRS[match]['tech_level'] - STUDENT_PAIRS[student]['tech_level']) <= 3:
                 print(f"{student} tech level={STUDENT_PAIRS[student]['tech_level']} with {match} tech level={STUDENT_PAIRS[match]['tech_level']}")
-                proceed = input('Okay to proceed? YES / NO: ')
+                should_proceed = input('Okay to proceed? [Y]ES / [N]O: ')
 
-                if proceed.lower() in ['yes', 'y']:
-                    # Set math creates a new set, so remove match from original pool
-                    students.remove(match)
-                elif proceed.lower() in ['no', 'n']:
+                if should_proceed.lower() in ['no', 'n']:
+                    # TODO break out matching as a function so it can be
+                    # called until (1) no tech level issue, (2) user approves 
+                    # match, or (3) run out of match options
                     print('Quitting: try again')
                     return
-
-            else:
-                students.remove(match)
+                while should_proceed.lower() not in ['yes', 'y']:
+                    should_proceed = input('Invalid key! [Y]ES / [N]O: ')
+            
+            # Set math creates a new set, so remove match from original pool
+            students.remove(match)
         except:
             print(f'Failed: students={students} student={student} match={match}')
+            return
 
         pairs[student] = match
 
     pprint(pairs)
+    should_write = input('Write to file? [Y]ES / [N]O: ')
     return pairs
 
-generate_pairs()
+# Don't think this will update the file correctly - because it will only write 
+# as a string...
+def update_global_var(pairs):
+    """Save generated pairs to global variable file"""
+
+    with open(FILE, 'w') as f:
+        for student in STUDENT_PAIRS:
+            pair = STUDENT_PAIRS[student]['past_pairs']
+            STUDENT_PAIRS[student]['past_pairs'].append(pair)
+            STUDENT_PAIRS[pair]['past_pairs'].append(student)
+            print(f"Updated student={student}['past_pairs']: {STUDENT_PAIRS[student]['past_pairs']}")
+            print(f"Updated pair={pair}['past_pairs']: {STUDENT_PAIRS[pair]['past_pairs']}")
+
+
+pairs = generate_pairs()
+
 
 
 # # All permutations
